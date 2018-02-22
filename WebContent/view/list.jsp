285705da362b9d750dbc219a8bde5faa87639722
zhdzhdgl <zhdzhdgl@gmail.com>
2018-02-22 17:52:28 +0900

    일기 게시판 DB테이블 생성, list 폼 만듬.

---
+++ WebContent/view/list.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>일기 리스트</h1>
+	<table>
+		<tr>
+			<th>No</th>
+			<th>일기제목</th>
+			<th>날짜</th>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>첫 일기</td>
+			<td>2018.02.22</td>
+		</tr>
+	</table>
+	<a href="write.jsp">일기쓰기</a>
+</body>
+</html>
---
+++ src/board/DiaryDataBean.java
@@ -0,0 +1,37 @@
+package board;
+
+import java.util.Date;
+
+public class DiaryDataBean {
+	private int num;
+	private String title;
+	private String content;
+	private Date date;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
+
+
